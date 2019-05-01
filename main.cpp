5fefcac766e21a41a872f3677a005862d953f961
faati <fatihaltinpinar@gmail.com>
2019-05-01 16:14:49 +0300

    Now has a potential to read text files.

--- main.cpp
+++ main.cpp
@@ -6,10 +6,23 @@
  *
  */
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
 int main(){
+    string x, x2;
+    char t;
+    ifstream inFile;
+    inFile.open("stock.txt");
+    int type, itemCount, price;
+    if(inFile.is_open()){
+        while(!inFile.eof()){
+            getline(inFile, x);
+            inFile >> x >> itemCount >> price;
+            cout << x << " " << itemCount << " " << price << endl;
+        }
+    }
 
     return 0;
 }
