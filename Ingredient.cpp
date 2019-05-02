d8288f0b70390627321f75c1d948eea4d0be7dd0
faati <fatihaltinpinar@gmail.com>
2019-05-02 15:15:24 +0300

    Class declaration and definitions for ingredients.

---
+++ Ingredient.cpp
@@ -0,0 +1,43 @@
+//
+// Created by faati on 01.05.2019.
+//
+
+#include "Ingredient.h"
+#include <iostream>
+
+using namespace std;
+
+Ingredient::Ingredient(const string name, const int type, const int item_count, const float price_per_unit) {
+    this->name = name;
+    this->type = type;
+    this->item_count = item_count;
+    this->price_per_unit = price_per_unit;
+}
+
+ostream &operator<<(ostream &os, const Ingredient &ingredient) {
+    ingredient.print(os);
+    return os;
+}
+
+
+float Ingredient::useIngredient(int amount) {
+    if(amount > item_count) throw ("Not enough " + name);
+    // TODO you cannot write an exception here, find a spot that you can write exception
+    //  This homework's quality level is way low.
+}
+
+
+ostream& Type1::print(ostream &os) const {
+    os << name << '\t' << 1 << '\t' << item_count << '\t' << price_per_unit;
+}
+
+
+ostream& Type2::print(ostream &os) const {
+    os << name << '\t' << 2 << '\t' << item_count << '\t' << price_per_unit;
+}
+
+
+ostream& Type3::print(ostream &os) const {
+    os << name << '\t' << 3 << '\t' << item_count << '\t' << price_per_unit;
+}
+
---
+++ Ingredient.h
@@ -0,0 +1,51 @@
+//
+// Created by faati on 01.05.2019.
+//
+#include <string>
+#include <exception>
+#ifndef INGREDIENT_H
+#define INGREDIENT_H
+
+using namespace std;
+
+class Ingredient {
+protected:
+    string name;
+    int type;
+    int item_count;
+    float price_per_unit;
+public:
+    Ingredient(string name,int type,int itemCount, float pricePerUnit);
+    friend ostream& operator<<(ostream &, const Ingredient &ingredient);
+    virtual ostream& print(ostream &os) const = 0;
+
+    // Function for using ingredients.
+    float useIngredient(int amount);
+
+    // Getters
+    string getName(){return name;}
+    int getType(){return type;}
+    int getItemCount(){return item_count;}
+    float getPricePerUnit(){return price_per_unit;}
+};
+
+class Type1: public Ingredient{
+public:
+    Type1(const string in_name, const int in_weight, const float in_price):Ingredient(in_name, 1, in_weight, in_price){}
+    ostream& print(ostream &os) const;
+};
+
+class Type2: public Ingredient{
+public:
+    Type2(const string in_name, const int in_weight, const float in_price):Ingredient(in_name, 2, in_weight, in_price){}
+    ostream& print(ostream &os) const;
+};
+
+class Type3: public Ingredient{
+public:
+    Type3(const string in_name, const int in_weight, const float in_price):Ingredient(in_name, 3, in_weight, in_price){}
+    ostream& print(ostream &os) const;
+};
+
+
+#endif //INGREDIENT_H
