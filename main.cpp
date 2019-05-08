7dde08c819a6fcda4cf3fe83c70dd445dcd6bcad
faati <fatihaltinpinar@gmail.com>
2019-05-08 05:43:43 +0300

    Added countLine function.

--- main.cpp
+++ main.cpp
@@ -17,27 +17,36 @@ const int MAX_INGREDIENT_COUNT = 10;
 
 using namespace std;
 
+int countLine(const string &file_name);
 
 
 int main(){
 
+    cout << countLine("menu.txt");
+
+    Stock x(100);
+    x.fillStock("stock.txt");
 
 
 
+    fstream stock_out("stock.txt");
+    stock_out << "Name\tType\tItemCount\tPrice\r\n";
+    stock_out << x;
 
 
-
-
-
-
-
-//    Stock x(100);             // TODO get stock size before creating stock.
-//    x.fillStock("stock.txt");
-//    fstream stock_out("stock.txt");
-//    stock_out << "Name\tType\tItemCount\tPrice\r\n";
-//    stock_out << x;
 
 
 
     return 0;
+}
+
+int countLine(const string &file_name){
+    int line_count = 0;
+    string tmp;
+    fstream file(file_name, ios::in);
+    while (!file.eof()){
+        getline(file, tmp);
+        line_count++;
+    }
+    return line_count;
 }
