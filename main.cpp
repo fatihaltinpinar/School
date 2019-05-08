a393569dae02f4c3c73b4828c36529bc14f5c3f1
faati <fatihaltinpinar@gmail.com>
2019-05-08 06:29:41 +0300

    Now can read menu.txt Added a function readMenu. Which reads the given txt file, treats it as a menu.txt file. With the information read, Product objects will be created and added to menu array, dynamically.

--- main.cpp
+++ main.cpp
@@ -18,15 +18,19 @@ const int MAX_INGREDIENT_COUNT = 10;
 using namespace std;
 
 int countLine(const string &file_name);
-
+void readMenu(string menu_file_name, Product* menu[]);
 
 int main(){
 
-    cout << countLine("menu.txt");
+    int product_count = countLine("menu.txt") - 1;
+    Product* menu[product_count];
+    readMenu("menu.txt", menu);
 
     Stock x(100);
     x.fillStock("stock.txt");
 
+    if (menu[0]->isAvailable(x))
+        cout << "test = " << menu[0]->serveProduct(x);
 
 
     fstream stock_out("stock.txt");
@@ -35,7 +39,8 @@ int main(){
 
 
 
-
+    for (int i = 0; i < product_count; i++)
+        delete menu[i];
 
     return 0;
 }
@@ -49,4 +54,37 @@ int countLine(const string &file_name){
         line_count++;
     }
     return line_count;
+}
+
+void readMenu(string menu_file_name, Product** menu){
+    fstream menu_file(menu_file_name, ios::in);
+
+    int product_count = 0;
+    stringstream ss;
+    string line;
+    string product_name;
+    string ingredient_info;
+    getline(menu_file, line);
+
+    while (!menu_file.eof()){
+        getline(menu_file, line);
+        ss.clear();
+        ss.str(line);
+        getline(ss, product_name, '\t');
+
+        menu[product_count] = new Product(product_name);
+
+        while (!ss.eof()){
+            getline(ss, ingredient_info, ',');
+            if (ingredient_info[0] == ' ')
+                ingredient_info.erase(0, 1);
+            if (ingredient_info[ingredient_info.length() - 1] == '\r')
+                ingredient_info.erase(ingredient_info.length() - 1, 1);
+            menu[product_count]->addIngredient(ingredient_info);
+        }
+        product_count++;
+    }
+
+
+
 }
